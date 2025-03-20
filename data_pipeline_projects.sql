00d522bde6010ec99d335806780f6ea6699df9b7
Emmanuel Justice <emmanueljustice25@gmail.com>
2025-03-20 14:31:49 +0100

    First commit of the sql file

---
+++ data_pipeline_projects.sql
@@ -0,0 +1,12 @@
+SELECT *
+FROM trans;
+
+SELECT cus.`Customer ID`, `Purchase Amount (USD)`,
+CASE 
+WHEN `Purchase Amount (USD)` >= 91 THEN 'High paying customer'
+WHEN `Purchase Amount (USD)` BETWEEN 50 AND 90 THEN 'Middle paying customer'
+Else 'Low paying customer'
+END AS Customer_profiling
+FROM customer cus
+INNER JOIN Trans Tra
+ON cus.`Customer ID` = tra.`customer ID`;
